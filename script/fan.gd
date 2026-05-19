ea243523f51ddf90fbb141753cae57e6aae9c0ff
Doells <irsyadr181@gmail.com>
2026-05-19 22:32:08 +0700

    feat: Obstacle Kipas

---
+++ script/fan.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+@export var push_force = -20000.0
+@export var active_time = 4.0
+@export var inactive_time = 2.0
+
+@onready var particles = $GPUParticles2D
+
+var is_active = true
+var timer = 0.0
+
+
+func _physics_process(delta):
+
+	timer += delta
+
+	# kondisi aktif
+	if is_active:
+
+		# nyalakan partikel
+		particles.emitting = true
+
+		# dorong player
+		for body in get_overlapping_bodies():
+			if body.has_method("apply_wind_force"):
+				body.apply_wind_force(push_force)
+
+		# timer aktif
+		if timer >= active_time:
+			is_active = false
+			timer = 0.0
+
+	# kondisi mati
+	else:
+
+		# matikan partikel
+		particles.emitting = false
+
+		# timer mati
+		if timer >= inactive_time:
+			is_active = true
+			timer = 0.0
--- script/player.gd
+++ script/player.gd
@@ -1,40 +1,47 @@
 extends CharacterBody2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var wind_force = 0.0
+
 
 func _physics_process(delta: float) -> void:
-	
+
 	# tambah animasi run dan idle
 	if velocity.x > 1 or velocity.x < -1:
 		animated_sprite_2d.animation = "run"
 	else:
 		animated_sprite_2d.animation = "idle"
-	# Add the gravity.
+
+	# gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# hidden jump sementara
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	# Ambil input A = KIRI dan D = Kanan
+	# input kiri kanan
 	var direction := Input.get_axis("move_left", "move_right")
-	#print(direction)
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# efek dorongan angin
+	velocity.x += wind_force * delta
+
 	move_and_slide()
-	
-	# ini untuk bagian flip kiri kanan angle karakter
+
+	# reset angin setelah dipakai
+	wind_force = 0
+
+	# flip karakter
 	if direction == 1.0:
 		animated_sprite_2d.flip_h = true
 	elif direction == -1.0:
 		animated_sprite_2d.flip_h = false
+
+
+func apply_wind_force(force):
+	wind_force = force
